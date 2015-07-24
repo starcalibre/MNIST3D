0b77e09ac16006d1baa6a5f4093b51c1a13863e9
Adrian Hecker <starcalibre@gmail.com>
2015-07-24 20:23:20 +1000

    Add as_dict method to Digit model

--- app/models.py
+++ app/models.py
@@ -1,13 +1,20 @@
 from app import db
 
 class Digit(db.Model):
+    __tablename__ = 'digits'
+
     id = db.Column(db.INTEGER, primary_key=True)
     label = db.Column(db.INTEGER)
     tsne_x = db.Column(db.REAL)
     tsne_y = db.Column(db.REAL)
     tsne_z = db.Column(db.REAL)
     array = db.Column(db.String)
-    image = db.Column(db.BLOB)
 
     def __repr__(self):
         return '<Digit %d %d>' % (self.id, self.label)
+
+    def as_dict(self, fields=None):
+        if not fields:
+            return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        else:
+            return {c: getattr(self, c) for c in fields}

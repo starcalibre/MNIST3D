63a4c26d56c2cfb1248a5dfc546ac1679ff94167
Adrian Hecker <starcalibre@gmail.com>
2015-07-25 10:08:31 +1000

    Mount 3d scatterplot with 1000 points in main script

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,1 +1,23 @@
-window.alert('Browserify works!');
+'use strict';
+/* exported scatterplot */
+
+var Scatterplot3 = require('./Scatterplot3');
+var scatterplot;
+
+var width = $('#scatterplotDiv').width();
+var height = $('#scatterplotDiv').height();
+
+var query = {
+    limit: 1000,
+    select: ['id', 'label', 'tsne_x', 'tsne_y', 'tsne_z']
+};
+
+$.ajax({
+    type: 'GET',
+    url: '/api?' + $.param(query),
+    success: function(data) {
+        console.log(data.result);
+        scatterplot = new Scatterplot3('scatterplotDiv', width,
+            height, data.result);
+    }
+});

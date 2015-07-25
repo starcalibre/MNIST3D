831db6792705dc025f1c3355c2bc9347b7422bf5
Adrian Hecker <starcalibre@gmail.com>
2015-07-25 13:54:03 +1000

    Setup main.js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,23 +1,46 @@
 'use strict';
-/* exported scatterplot */
+/* exported scatterplot, DigitCanvas, digitCanvas */
 
 var Scatterplot3 = require('./Scatterplot3');
+var DigitCanvas = require('./DigitCanvas');
+
 var scatterplot;
 
-var width = $('#scatterplotDiv').width();
-var height = $('#scatterplotDiv').height();
+var scatterPlotWidth = $('#scatterplotDiv').width();
+var scatterPlotHeight = $('#scatterplotDiv').height();
+var digitCanvasWidth = $('#canvasDiv').width();
+var digitCanvasHeight = $('#canvasDiv').width();
 
 var query = {
-    limit: 1000,
+    limit: 10000,
     select: ['id', 'label', 'tsne_x', 'tsne_y', 'tsne_z']
 };
 
+var digitCanvas = new DigitCanvas('digitCanvas', digitCanvasWidth,
+    digitCanvasHeight);
+
 $.ajax({
     type: 'GET',
-    url: '/api?' + $.param(query),
+    url: '/api?' + $.param(query, true),
     success: function(data) {
-        console.log(data.result);
-        scatterplot = new Scatterplot3('scatterplotDiv', width,
-            height, data.result);
+        console.log(data.result.length);
+        scatterplot = new Scatterplot3('scatterplotDiv', scatterPlotWidth,
+            scatterPlotHeight, data.result);
     }
+});
+
+$('body').on('updatePoint', function(event, data) {
+    var query = {
+        id: data,
+        select: ['array']
+    };
+
+    $.ajax({
+        type: 'GET',
+        url: '/api?' + $.param(query, true),
+        success: function(data) {
+            var digitArray = JSON.parse('[' + data.result[0].array + ']');
+            digitCanvas.draw(digitArray[0]);
+        }
+    });
 });

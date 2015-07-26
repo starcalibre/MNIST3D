29ed9009138b08845e4a046562d31aa9e3ea001d
Adrian Hecker <starcalibre@gmail.com>
2015-07-26 18:16:50 +1000

    Add resize event to digit canvas

--- app/static/js/DigitCanvas.js
+++ app/static/js/DigitCanvas.js
@@ -4,12 +4,10 @@ var Utils = require('./Utils');
 
 function DigitCanvas(element, width, height) {
     this.canvas = document.getElementById(element);
-    this.canvas.width = width;
-    this.canvas.height = height;
     this.context = this.canvas.getContext('2d');
+    this.lastValue = null;
 
-    this.gridWidth = this.canvas.width/28;
-    this.gridHeight = this.canvas.height/28;
+    this.setSize(width, height);
 }
 
 DigitCanvas.prototype.draw = function(valueArray) {
@@ -18,13 +16,29 @@ DigitCanvas.prototype.draw = function(valueArray) {
         var cellY = Math.floor(i / 28);
         this.drawCell(valueArray[i], cellX, cellY);
     }
+    this.lastValue = valueArray;
 };
 
 DigitCanvas.prototype.drawCell = function(value, cellX, cellY) {
     this.context.fillStyle = Utils.rgbToHex(255 - value,
         255 - value, 255 - value);
+    this.context.lineWidth = 0;
     this.context.fillRect(this.gridWidth * cellX, this.gridHeight * cellY,
         this.gridWidth, this.gridHeight);
+};
+
+DigitCanvas.prototype.setSize = function(width, height) {
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.gridWidth = this.canvas.width/28;
+    this.gridHeight = this.canvas.height/28;
+};
+
+DigitCanvas.prototype.resize = function(width, height) {
+    this.setSize(width, height);
+    if(this.lastValue) {
+        this.draw(this.lastValue);
+    }
 };
 
 module.exports = DigitCanvas;
